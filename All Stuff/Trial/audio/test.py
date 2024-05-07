7f61ea30e2485cadec3f710e8bc7b99055b169ce
Aryan Patel <82220795+Aryan185@users.noreply.github.com>
2024-05-07 13:32:18 +0530

    Delete All Stuff directory

--- All Stuff/Trial/audio/test.py
+++
@@ -1,8 +0,0 @@
-import ffmpeg
-
-# Input MP3 file and output WAV file
-input_file = "aud_latest.wav"
-output_file = "output_file.wav"
-
-# Run ffmpeg command
-ffmpeg.input(input_file).output(output_file).run()
